76687480e2479a69f20223aa361d6c9efaa4c280
Ignacio <jakrich@cifpfbmoll.eu>
2022-02-10 18:46:31 +0100

    modelo post creado

--- examen2eva-10/app/Models/Post.php
+++ examen2eva-10/app/Models/Post.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $table = 'posts';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'title',
+        'create-date',
+        'publi-date',
+        'options',
+        'extract',
+        'content',
+        'access',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected $visible = [
+        'title',
+        'create-date',
+        'publi-date',
+        'options',
+        'extract',
+        'content',
+        'access',
+        'user_id',
+    ];
 }
