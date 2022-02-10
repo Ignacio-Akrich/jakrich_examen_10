ac0ebd93fa12f26ea0275ae80d2a5198cfda2d62
Ignacio <jakrich@cifpfbmoll.eu>
2022-02-10 18:26:51 +0100

    formularios + old values

---
+++ examen2eva-10/resources/views/posts/edit.blade.php
@@ -0,0 +1,53 @@
+@extends('posts.layout')
+@section('content')
+<body>
+<h1>POSTS</h1> <span>@lang('Create publication')</span>
+@if($errors->any)
+        <div>
+            <ul class="list-group">
+                @foreach($errors->all() as $e)
+                        <li class="list-group-item list-group-item-danger m-1">{{ $e }}</>
+                @endforeach
+            </ul>
+        </div>
+@endif
+<form action="{{route('posts.update',$post->id)}}" class="card card-body bg-secondary" method="POST">
+        @csrf
+        @method('POST')
+        <h3>Create a publication</h3>
+        <div class="mb-3">
+        <label class="form-label">@lang('Title of the publication')</label>
+        <input type="text" class="form-control" title="title" placeholder="Title" value="{{ old('title') }}" required>
+        <div class="mb-3">
+        <label class="form-label">@lang('Create date')</label>
+        <input type="date" class="form-control" title="create-date" value="{{old('create-date')}}" required>
+        </div>
+         <div class="mb-3">
+        <label class="form-label">@lang('Publication date')</label>
+        <input type="date" class="form-control" title="publi-date" value="{{old('publi-date')}}" required>
+        </div>
+        <div class="mb-3">
+        <label class="form-label">@lang('Options')</label>
+        <div class="mb-3">
+        <input class="form-check-input" type="checkbox" title="options[]" value="Caducable" {{ (is_array(old('options')) and in_array('Caducable', old('options'))) ? 'checked' : '' }}>
+        <label class="form-check-label">@lang('Caducable')</label>
+        <input class="form-check-input" type="checkbox" title="optionss[]" value="Comentable" {{ (is_array(old('options')) and in_array('Comentable', old('options'))) ? 'checked' : '' }}>
+        <label class="form-check-label">@lang('Comentable')</label>
+        </div>
+        </div>
+        <div class="mb-3">
+        <label class="form-label">@lang('Publication extract')</label>
+        <div class="mb-3">
+        <textarea class="form-control" title="extract" rows="2" value="{{old('extract')}}" required></textarea>
+        </div>
+        </div>
+         <div class="mb-3">
+        <label class="form-label">@lang('Publication content')</label>
+        <div class="mb-3">
+        <textarea class="form-control" title="content" rows="5" value="{{old('content')}}" required></textarea>
+        </div>
+        </div>
+        
+        <input type="submit" class="btn btn-primary" value="Enviar">
+  </body>  
+@endsection
---
+++ examen2eva-10/resources/views/posts/index.blade.php
@@ -0,0 +1,33 @@
+@extends('posts.layout')
+@section('content')
+<a href="{{route('posts.create')}}" class="btn btn-primary">Create</a>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>@lang('Title')</th>
+                <th>Actions</th>
+            </tr>
+
+        </thead>
+        <tbody>
+        @foreach($posts as $post)
+            <tr>
+                <td>{{ $post->name}}</td>
+                <td>{{ $post->types }}</td>
+                <td> 
+                <form action="{{route('posts.destroy',$post->id)}}" method="POST">
+               {{--   @can('isAdmin')  --}}
+                    @csrf
+                    @method('DELETE')
+                    <a href="{{route('posts.edit', $post->id)}}" class="btn btn-warning">@lang('Edit')</a>
+                    <button type="submit" class="btn btn-danger">@lang('Delete')</button>
+               {{--   @endcan  --}}
+                </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
+@endsection
